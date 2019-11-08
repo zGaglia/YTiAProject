b2c47e8f568f3fd21a0c5de6ae4768dd2b168072
SDK-840-121 <gifuionut9519@yahoo.com>
2019-11-08 11:22:05 +0100

    transition0.0.2

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
+  animations: [
+    trigger('transitionTest', [
+      state('void', style({
+        transition : 0.4,
+        opacity: 0
+      })),
+      transition('void <=> *', animate(1000)),
+    ]),
+  ]
 })
 export class MainComponent implements OnInit {
 
   constructor() { }
+  currentState: boolean = true;
 
   ngOnInit() {
   }
